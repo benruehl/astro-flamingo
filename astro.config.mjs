24de0248263dd45cb23a9989eb937ef2de879741
Benjamin Rühl <dev.ruehl@gmail.com>
2024-05-15 22:56:10 +0200

    feat: update animation on mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    scopedStyleStrategy: 'class'
+    scopedStyleStrategy: 'class',
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "./src/styles/main.scss";',
+                },
+            },
+        },
+    },
 });
