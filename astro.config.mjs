facdcc65c9773669a8a45fdae0aefad448756187
Benjamin Rühl <dev.ruehl@gmail.com>
2024-05-18 20:15:00 +0200

    feat: add icon cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    scopedStyleStrategy: 'class',
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: '@import "./src/styles/main.scss";',
-                },
-            },
-        },
-    },
+  scopedStyleStrategy: 'class',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "./src/styles/main.scss";'
+        }
+      }
+    }
+  },
+  integrations: [icon()]
 });
